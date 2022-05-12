e411cc20decd837689860f4d5c563319414e0a59
Олег Кузнецов <zyrker1@gmail.com>
2022-05-12 12:36:39 +0300

    update user

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from users.models import User
 from .serializers import UserSerializer
@@ -6,3 +6,5 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', ]
